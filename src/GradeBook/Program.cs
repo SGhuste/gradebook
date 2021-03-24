c2118c7a6469e6e728d0038929444799cf0c7695
HEXQ-7\sghus <sghuste@epsilonsystems.com>
2021-03-24 10:26:06 -0700

    Initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book("Shane");
+      book.AddGrade(89.1);
+      book.AddGrade(90.5);
+      book.AddGrade(77.1);
+
+      var stats = book.GetStatistics();
+
+      Console.WriteLine($"The lowest grade is {stats.Low}");
+      Console.WriteLine($"The highest grade is {stats.High}");
+      Console.WriteLine($"The average grade is {stats.Average:N1}");
+    }
+  }
+}
